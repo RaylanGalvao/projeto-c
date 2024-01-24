1d39e6d0dab7069c8ade3721e093485787874341
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-01-24 11:19:58 -0300

    exercicio 16

---
+++ exercicios/ex015.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    n=0; 
+    while (n<10){
+        cout << n << endl;
+        n++;
+        //cin >> n;
+    }   
+    cout << endl <<"rotina finalizada" << endl;
+    return 0;
+}
