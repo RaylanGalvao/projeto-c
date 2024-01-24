d95234940842a736b81f94b9372678710314126b
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-01-24 13:50:37 -0300

    exercicio 18

--- exercicios/ex016.cpp
+++ exercicios/ex016.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main(){
-    
+    int cont;
+    cont =0;
+    while(cont < 1000){
+        cout << cont << endl;
+        cont++;
+    }
 
 
     return 0;
---
+++ exercicios/ex018.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /*
+    for(iniciacao; condicional; incremento/decremento){
+        comandos
+    }
+    */
+    int x;
+    return 0;
+}
