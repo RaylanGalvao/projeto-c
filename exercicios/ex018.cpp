69618e1a4a05105f9cb5932d4d34accf6d946606
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-01-24 16:35:24 -0300

    ex 018

--- exercicios/ex018.cpp
+++ exercicios/ex018.cpp
@@ -8,6 +8,10 @@ int main(){
         comandos
     }
     */
-    int x;
+    int x,y,z;
+
+    for (x=0,y=1,z=0; x<=10,z<=6; x++,y+=2,z+=2){
+        cout << x << "-" << y << "-" << z << endl;
+    }
     return 0;
 }
