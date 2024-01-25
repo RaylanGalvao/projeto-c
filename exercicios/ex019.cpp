06d80ab11c8a593cfc3cedbf4053f3eafdf7e3a7
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-01-25 11:08:20 -0300

    criei ex020

---
+++ exercicios/ex019.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int vetor[5];
+    int i;
+    vetor[0]=10;
+    vetor[1]=20;
+    vetor[2]=30;
+    vetor[3]=40;
+    vetor[4]=50;
+    for(i=0; i<5; i++){
+    cout << vetor [i] << endl; 
+    }
+    cout << vetor [0] << endl; 
+    cout << vetor [1] << endl; 
+    cout << vetor [2] << endl; 
+    cout << vetor [3] << endl; 
+    cout << vetor [4] << endl; 
+
+    return 0;
+}
